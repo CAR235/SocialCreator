ef8d8902bc1403232b2694b4fffbfca627f982a6
CAR235 <30805156+CAR235@users.noreply.github.com>
2025-05-31 01:58:45 +0200

    DIOCANE

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './LandingPage';
 import App from './App';
 
 const AppRouter = () => {
-  return <App />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/app" element={<App />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default AppRouter;
