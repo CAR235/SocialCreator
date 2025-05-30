32a787d68436388a03d2fee176882f8d64455763
CAR235 <30805156+CAR235@users.noreply.github.com>
2025-05-31 01:36:25 +0200

    Fix GitHub Pages routing

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './LandingPage';
 import App from './App';
 
 const AppRouter = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/app" element={<App />} />
-      </Routes>
-    </Router>
-  );
+  return <App />;
 };
 
 export default AppRouter;
